726c92dc4ff1dad2b9a5c50f42363349c9c184d4
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-06 14:43:35 -0400

    server ready for image dropping

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('_middleware/error-handler');
+const multer = require('multer');
+const path = require('path');
+const ejs = require('ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,6 +17,42 @@ app.use('/users', require('./users/users.controller'));
 
 // global error handler
 app.use(errorHandler);
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+  
+  // Init Upload
+  const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+  });
+  
+  
+  // EJS
+  app.set('view engine', 'html');
+  
+  // Public Folder
+  app.use(express.static('./public'));
+  
+//   app.get('/', (req, res) => console.log("hi"));
+  
+
+var type = upload.single('myImage');
+
+app.post('/upload', type, (req, res, next) => {
+    const file = req.file
+    if (!file) {
+      const error = new Error('Please upload a file')
+      error.httpStatusCode = 400
+      return next(error)
+    }
+      res.send(file)
+    
+  })
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
