81b157e8b1ceeff9e2611f26ee90ed2f10beabe0
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-09 01:51:57 -0400

    changed db.js

--- _helpers/db.js
+++ _helpers/db.js
@@ -8,8 +8,8 @@ initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
+    const { host, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, user, password, database });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
