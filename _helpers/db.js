67cc4facc99ab30e203e342c45db6b3363cb17b0
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-09 02:07:58 -0400

    revert a few things

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,14 +2,20 @@ const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
-module.exports = db = {};
+// module.exports = db = {};
+module.exports = db =  {
+    HOST: "us-cdbr-east-03.cleardb.com",
+    USER: "b1f6bd6150b66f",
+    PASSWORD: "55daa557",
+    DB: "heroku_931ecfbffb230c4"
+  };
 
 initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, user, password, database });
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
