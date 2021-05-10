363102b09397d40c4f32fbbd1cdbb06d911adea7
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-10 00:03:44 -0400

    add port back

--- _helpers/db.js
+++ _helpers/db.js
@@ -9,8 +9,8 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
 
-    const { host, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, user, password });
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
