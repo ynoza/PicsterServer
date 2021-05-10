88f8992f803407b458f179e843c681862d59be56
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-09 22:56:41 -0400

    moving sequalize.drop

--- _helpers/db.js
+++ _helpers/db.js
@@ -8,14 +8,15 @@ initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    await sequelize.drop()
-    
+
     const { host, port, user, password, database } = config.database;
     const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
     const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+    await sequelize.drop()
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
