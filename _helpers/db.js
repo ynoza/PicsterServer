e5c2649b6c6884f626649fb66812710d464c11c7
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-09 20:32:21 -0400

    tried removing port

--- _helpers/db.js
+++ _helpers/db.js
@@ -3,19 +3,14 @@ const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
-// module.exports = db =  {
-//     HOST: "us-cdbr-east-03.cleardb.com",
-//     USER: "b1f6bd6150b66f",
-//     PASSWORD: "55daa557",
-//     DB: "heroku_931ecfbffb230c4"
-//   };
 
 initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
+    const { host, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, user, password });
+    console.log("did sql connection")
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
@@ -23,7 +18,7 @@ async function initialize() {
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
-    console.log(host,port,user,password,database);
+    console.log(host, user,password,database);
     // sync all models with database
     await sequelize.sync();
 }
