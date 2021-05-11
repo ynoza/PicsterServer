310500c6be1856cbfc2f974d596043639b302b90
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-11 00:01:14 -0400

    AI attempt 3

--- img/img.controller.js
+++ img/img.controller.js
@@ -11,8 +11,9 @@ const herokuServerLink='https://picsterserver.herokuapp.com/public/uploads/';
 
 const serverInUse=herokuServerLink;
 
-// let classficationToImagesMap = new Map();
-// initialAddClassificationDataToMap();
+let classficationToImagesMap = new Map();
+initialAddClassificationDataToMap();
+
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
@@ -48,7 +49,7 @@ router.post('/getUploads', function(req, res){
 // handles the addition of new images
 router.post('/upload', upload.single('myImage'), async (req, res, next) => {
     const file = req.file;
-    // intermediateClassificationDataToMap(file.originalname);
+    intermediateClassificationDataToMap(file.originalname);
     if (!file) {
       const error = new Error('Please upload a file')
       error.httpStatusCode = 400
@@ -67,12 +68,12 @@ router.post('/delete', (req, res, next) => {
             break;   
         }
     }
-    // try {
-    //   removeClassificationDataToMap(str);
-    // }
-    // catch {
-    //   console.log("unable to remove Image from Map")
-    // }
+    try {
+      removeClassificationDataToMap(str);
+    }
+    catch {
+      console.log("unable to remove Image from Map")
+    }
 
 
     // part that connects to local storage and removes the file
@@ -87,14 +88,13 @@ router.get('/imagesAndMapPair', function(req, res){
   const folder = './public/uploads/';
     fs.readdir(folder, (err, files) => {
       files.forEach(file => {  
-        //   if (classficationToImagesMap.has(file)){
+          if (classficationToImagesMap.has(file)){
             var url = encodeURI(serverInUse+file);
-            // let imageDesc = classficationToImagesMap.get(file);
-            let imageDesc = ['AI TEMP BLOCKED'];
+            let imageDesc = classficationToImagesMap.get(file);
             console.log(imageDesc)
             const obj = { imageURL: url, desc: imageDesc}
             lst.push(obj);
-        //   }
+          }
       });
       res.send(lst);
     });
@@ -102,76 +102,75 @@ router.get('/imagesAndMapPair', function(req, res){
 
 
 
-// // Reading an Image for Image Classification
-// const readImage = path => {
-//   const imageBuffer = fs.readFileSync(path);
-//   const tfimage = tfnode.node.decodeImage(imageBuffer);
-//   return tfimage;
-// }
+// Reading an Image for Image Classification
+const readImage = path => {    
+    const imageBuffer = fs.readFileSync(path);
+    const tfimage = tfnode.tidy(() => tfnode.node.decodeImage(imageBuffer) );
+    console.log(tfimage);
+    return tfimage;
+}
 
-// // Actually doing the classfication for Image Classification
-// const imageClassification = async path => {
-//   const image = readImage(path);
-//   // Load the model.
-//   const mobilenetModel = await mobilenet.load();
-//   // Classify the image.
-//   let newPredictions=[];
-//   try {
-//     const predictions = await mobilenetModel.classify(image);
-//     newPredictions = getClassNames(predictions);
-//   }
-//   catch(err) {
-//     console.log(err);
-//     newPredictions = 'Cannot classify this image because of its size or its format';
-//   }
-//   // console.log('Classification Results:', predictions);
-//   return newPredictions;
-// }
+// Actually doing the classfication for Image Classification
+const imageClassification = async path => {
+  const image = readImage(path);
+  // Load the model.
+  const mobilenetModel = await mobilenet.load();
+  // Classify the image.
+  let newPredictions=[];
+  try {
+    const predictions = await mobilenetModel.classify(image);
+    newPredictions = getClassNames(predictions);
+  }
+  catch(err) {
+    console.log(err);
+    newPredictions = 'Cannot classify this image because of its size or its format';
+  }
+  // console.log('Classification Results:', predictions);
+  return newPredictions;
+}
 
-// const getClassNames =  (predictions) => {
-//   // console.log(predictions);
-//   let newPredictions=[];
-//   predictions.forEach( (pair) => {
-//     pair['className'].split(',').forEach( (potentialDesc) => {
-//         if (potentialDesc.length > 0){
-//           newPredictions.push(potentialDesc);
-//         }
-//     })
-//   })
-//   // console.log(newPredictions);
-//   return newPredictions;
-// }
+const getClassNames =  (predictions) => {
+  // console.log(predictions);
+  let newPredictions=[];
+  predictions.forEach( (pair) => {
+    pair['className'].split(',').forEach( (potentialDesc) => {
+        if (potentialDesc.length > 0){
+          newPredictions.push(potentialDesc);
+        }
+    })
+  })
+  // console.log(newPredictions);
+  return newPredictions;
+}
 
 
-// function initialAddClassificationDataToMap(){
-//     const folder = './public/uploads/';
-//     fs.readdir(folder, (err, files) => {
-//       files.forEach(async file => {
-//         intermediateClassificationDataToMap(file);
-//       })
-//     })
-//     console.log(classficationToImagesMap);
-// }
+function initialAddClassificationDataToMap(){
+    const folder = './public/uploads/';
+    fs.readdir(folder, (err, files) => {
+      files.forEach(async file => {
+        intermediateClassificationDataToMap(file);
+      })
+    })
+    console.log(classficationToImagesMap);
+}
 
-// async function intermediateClassificationDataToMap(file) {
-//   return new Promise(function (resolve, reject) {
-//     const newPredictions = imageClassification('./public/uploads/' + file); 
-//     console.log(classficationToImagesMap);
-//     newPredictions.then( function(result) {
-//       classficationToImagesMap.set(file, result);    
-//       resolve(file);
-//     })
-//     // when you do a log here it doesnt wait for the result of newPredictions so it will redturn a pending Promise
-//     // console.log(newPredictions);
-//   })
-// }
+function intermediateClassificationDataToMap(file) {
+    const newPredictions = imageClassification('./public/uploads/' + file); 
+    console.log(classficationToImagesMap);
+    newPredictions.then( function(result) {
+      classficationToImagesMap.set(file, result);
+      console.log(result);    
+    })
+    // when you do a log here it doesnt wait for the result of newPredictions so it will redturn a pending Promise
+    // console.log(newPredictions);
+}
 
-//   function removeClassificationDataToMap(file){
-//     if (classficationToImagesMap.has(file)){
-//       classficationToImagesMap.delete(file);
-//     }
-//     console.log(classficationToImagesMap);
-//   }
+  function removeClassificationDataToMap(file){
+    if (classficationToImagesMap.has(file)){
+      classficationToImagesMap.delete(file);
+    }
+    console.log(classficationToImagesMap);
+  }
 
 
   module.exports = router;
