1c9ccfd2963a5d9c4ade229404240b505ae6182a
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-10 20:26:51 -0400

    change entrypoint

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://192.168.2.21:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://ynoza.github.io/Picster"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
